1835bbe81aeba35bb76af891219c8e2784eeb61d
gomijoa <songforyou85@gmail.com>
2024-07-01 13:05:09 +0800

    homework-calculator

---
+++ app.js
@@ -0,0 +1,22 @@
+const calculator = {
+  add: function (a, b) {
+    console.log(a + b);
+  },
+  minus: function (a, b) {
+    console.log(a - b);
+  },
+  divide: function (a, b) {
+    console.log(a / b);
+  },
+  multi: function (a, b) {
+    console.log(a * b);
+  },
+  power: function (a, b) {
+    console.log(a ** b);
+  },
+};
+calculator.add(1, 1);
+calculator.minus(45, 5);
+calculator.divide(54, 9);
+calculator.multi(3, 12);
+calculator.power(4, 4);
